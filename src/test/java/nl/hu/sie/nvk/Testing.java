a513a8ecd024d3ea9efb3291b7625a3f2a7a9f99
Nynke van Koningsveld <NynkevK@users.noreply.github.com>
2019-05-06 15:04:02 +0200

    packagecloud distribution added

--- src/test/java/nl/hu/sie/nvk/Testing.java
+++ src/test/java/nl/hu/sie/nvk/Testing.java
@@ -1,8 +1,12 @@
 package nl.hu.sie.nvk;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class Testing {
 
-    @Test
+    @Test @Disabled
     public void test_fail() {
         fail("Let's test the failing unit test");
     }
