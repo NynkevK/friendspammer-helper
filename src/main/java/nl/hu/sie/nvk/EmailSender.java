8f29a289e6a77cc6507c271d5deb8472b8a79a4c
Nynke van Koningsveld <NynkevK@users.noreply.github.com>
2019-05-22 09:40:16 +0200

    EmailSender codesmells refactored

--- src/main/java/nl/hu/sie/nvk/EmailSender.java
+++ src/main/java/nl/hu/sie/nvk/EmailSender.java
@@ -12,6 +12,8 @@ import javax.mail.internet.MimeMessage;
 
 public class EmailSender {
 
+    private EmailSender(){ }
+
     public static void sendEmail(String subject, String to, String messageBody, boolean asHtml) {
 
         Properties props = new Properties();
@@ -45,50 +47,8 @@ public class EmailSender {
 
             MongoSaver.saveEmail(to, "spammer@spamer.com", subject, messageBody, asHtml);
 
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
         }
     }
-
-//    public static void sendEmail(String subject, String[] toList, String messageBody, boolean asHtml) {
-//
-//        Properties props = new Properties();
-//        props.put("mail.smtp.host", "smtp.mailtrap.io");
-//        props.put("mail.smtp.port", "2525");
-//        props.put("mail.smtp.auth", "true");
-//
-//        final String username = "1a9a445ba8a16a";
-//        final String password = "d247bbf1e337cb";
-//
-//        Session session = Session.getInstance(props,
-//                new javax.mail.Authenticator() {
-//                    protected PasswordAuthentication getPasswordAuthentication() {
-//                        return new PasswordAuthentication(username, password);
-//                    }
-//                });
-//        try {
-//
-//            for (int index = 0; index < toList.length; index++) {
-//
-//                Message message = new MimeMessage(session);
-//                message.setFrom(new InternetAddress("spammer@spammer.com"));
-//                message.setRecipients(Message.RecipientType.TO,
-//                        InternetAddress.parse(toList[index]));
-//                message.setSubject(subject);
-//
-//                if (asHtml) {
-//                    message.setContent(messageBody, "text/html; charset=utf-8");
-//                } else {
-//                    message.setText(messageBody);
-//                }
-//                Transport.send(message);
-//
-//                System.out.println("Done");
-//            }
-//
-//        } catch (MessagingException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-
 }
